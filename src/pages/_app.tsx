239933f591e9a0cd4f70c0348783a704b716d03d
eduardomarr <dado.nep@gmail.com>
2023-03-13 14:39:57 -0300

    header builded

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,21 @@
-import { globalStyles } from '@/styles/global'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app"
+import { globalStyles } from "../styles/global"
+
+import logoImg from "../assets/logo.svg"
+import { Container, Header } from "../styles/pages/app"
 
 globalStyles()
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }: AppProps) {
+    return (
+    <Container>
+      <Header>
+        <img src={logoImg.src} alt="" />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  )
 }
+
+export default App
